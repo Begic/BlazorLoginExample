281ec72960e51b5dfa84dc86c297ad786d05dee1
Süleyman Begic <begic@software.tirol>
2024-02-24 22:54:10 +0100

    update

--- BlazorLoginExample/Program.cs
+++ BlazorLoginExample/Program.cs
@@ -1,7 +1,33 @@
+using AspNetCore.Authentication.Basic;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddAuthentication(BasicDefaults.AuthenticationScheme)
+    .AddBasic(options =>
+    {
+        options.Realm = "My Realm";
+        options.Events = new BasicEvents()
+        {
+            OnValidateCredentials = context =>
+            {
+                // Hier können Sie Ihre eigene Logik zur Überprüfung der Anmeldeinformationen implementieren
+                if (context.Username == "username" && context.Password == "password")
+                {
+                    context.Principal = new System.Security.Claims.ClaimsPrincipal();
+                    context.Success();
+                }
+                else
+                {
+                    context.NoResult();
+                }
+
+                return Task.CompletedTask;
+            }
+        };
+    });
 
 var app = builder.Build();
 
@@ -19,5 +45,14 @@ app.UseRouting();
 
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.MapRazorPages();
 
 app.Run();
